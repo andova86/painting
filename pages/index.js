9479e4777c07191785ab98a5dd3f2e90e86c04fc
andova86 <andova86@gmail.com>
2020-11-05 16:37:00 -0500

    Cambio de Icono y Textos en el home

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,46 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pinturas de Martha</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Ya empezamos <a href="https://nextjs.org">Churi</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Empezamos el desarollo de la pagina de pinturas
+          
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Pinturas &rarr;</h3>
+            <p>La pintura es el arte de la representación gráfica utilizando pigmentos mezclados con otras sustancias aglutinantes orgánicas o sintéticas. Aqui encontraras todo los detalles de la autora</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Acerca de  &rarr;</h3>
+            <p>Toda la informacion referida a la autora de las pinturas mostradas en nuestro sistio</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Galeria &rarr;</h3>
+            <p>La pintura conjuga elementos de la representación plástica como las formas, los colores, las texturas, la armonía, el equilibrio, la perspectiva, la luz y el movimiento. Aquí descubriras las mejores pinturas creadas por la autora en una galeria de fotos</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Contacto &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Mediante este enlace podraas contactar con la autora de las pinturas.
             </p>
           </a>
         </div>
@@ -57,7 +57,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/team.svg" alt="DevTeam Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
